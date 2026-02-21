48de8896d3872c2f6e2e1d294fc46218c6f8a719
Yetch <103007836+theyetch@users.noreply.github.com>
2026-02-20 18:20:28 -0800

    Renewal

--- script.js
+++
@@ -1,7 +0,0 @@
-const image = document.getElementById('soundTrigger');
-const audio = document.getElementById('gnooshk');
-
-image.addEventListener('click', () => {
-    audio.currentTime = 0; // Rewind to start
-    audio.play();
-});
